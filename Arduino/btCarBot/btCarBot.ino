e6624cf3113213bcca116aac34ac9a3381299f4b
Royal Fort <RoyalFort@users.noreply.github.com>
2014-02-17 21:54:33 -0700

    Adding help message
    Replacing code with "new" version, adding help menu, and changing pins
    for bluetooth to my current breadboard setup.

--- Arduino/btCarBot/btCarBot.ino
+++ Arduino/btCarBot/btCarBot.ino
@@ -1,90 +1,153 @@
 #include <SoftwareSerial.h>
- 
-SoftwareSerial mySerial(2, 3); // RX, TX
+
+SoftwareSerial mySerial(8, 14); // RX, TX
 
 const int leftFwd = 9; // green
 const int leftRev = 10; // blue
 const int rightFwd = 5; // grey
 const int rightRev = 6; // purple
 
-boolean ran = false;
-byte velocity = 255;
+boolean ran = false; // What is this for?
+byte velocity = 255; // I assume this will be the default speed
+int commandDelay = 20;
 
-void setup()  
-{
-  // Open serial communications and wait for port to open:
-  Serial.begin(57600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
-  }
+/* Ethan's config idea:
+  By pressing the "c" key, you leave drive(d) mode enter configuration(c) mode:
+  Once in (c) mode, you can press the following key to change a corresponding option:
+	- v for changing velocity
+	- Add more config options later if we need to
+  Followed by pressing the "d" key (for "done"), which would put you back into drive(d) mode
+  
+  Maybe it's a bit confusing, but an android app could have a front-end to this, allowing the user
+  to make a speed selection, and the app would output all these key codes automatically. (User would
+  never know they left drive mode)
+  
+*/
 
-  Serial.println("Start your engines!");
+// d = drive
+// c = config
+// o = option-set mode
 
-  // set the data rate for the SoftwareSerial port
-  mySerial.begin(9600);
-  mySerial.println("Hello, world?");
+char mode = 'd';
+char option;
+
+void setup() {
+	// Open serial communications and wait for port to open:
+	Serial.begin(57600);
+	while (!Serial) {
+          ; // wait for serial port to connect. Needed for Leonardo only
+	}
+
+	Serial.println("Start your engines!");
+
+	// set the data rate for the SoftwareSerial port
+	mySerial.begin(9600);
+	mySerial.println("Hello, world?");
 }
 
-void loop() // run over and over
-{
-  byte b = 0;
-  if (mySerial.available())
-    b = mySerial.read();
-  
-  if (b != 0)
-    Serial.write(b);
-  else
-    return;
-
-  // Not connected right now
-  /*if (Serial.available())
-    mySerial.write(Serial.read());*/
-  
-  int commandDelay = 20;
-  switch (b)
-  {
-    case 'F':
-    case 'w':
-      Serial.println("Forward");
-      analogWrite(leftFwd, velocity);
-      analogWrite(rightFwd, velocity);
-      delay(commandDelay);
-      analogWrite(leftFwd, 0);
-      analogWrite(rightFwd, 0);
-      break;
-    case 'B':
-    case 's':
-      Serial.println("Backward");
-      analogWrite(leftRev, velocity);
-      analogWrite(rightRev, velocity);
-      delay(commandDelay);
-      analogWrite(leftRev, 0);
-      analogWrite(rightRev, 0);
-      break;
-    case 'L':
-    case 'a':
-      Serial.println("Left");
-      analogWrite(leftRev, velocity);
-      analogWrite(rightFwd, velocity);
-      delay(commandDelay);
-      analogWrite(leftRev, 0);
-      analogWrite(rightFwd, 0);
-      break;
-    case 'R':
-    case 'd':
-      Serial.println("Right");
-      analogWrite(leftFwd, velocity);
-      analogWrite(rightRev, velocity);
-      delay(commandDelay);
-      analogWrite(leftFwd, 0);
-      analogWrite(rightRev, 0);
-      break;
-    default:
-      Serial.print("Set speed: ");
-      Serial.println(b);
-      if (b > 0 && b < 255)
-        velocity = b;
-      break;
+void loop() {
+	
+	// Don't do anything unless there was input from the user
+	if (mySerial.available()) {
+	
+		byte character = mySerial.read();
+		Serial.write(character);
+		
+                // Change modes, skip processing until further keys are entered
+                switch(character) {
+			case 'p':
+			case 'o':
+			case 'c':
+                          mode = character;
+                          return;
+                        case 'h': // Not a mode, just a message
+                          printHelp();
+                          return;
+                }
+		
+		switch (mode) {
+			case 'p': driveCommand(character); break;
+			case 'o': optionSet(character); break;
+			case 'c': config(character); break;
+		}
+	}
+  if (Serial.available()) {
+    byte read = Serial.read();
+    mySerial.write(read);
   }
 }
 
+void printHelp() {
+  mySerial.println("----===== HELP =====----");
+  mySerial.println("Modes:");
+  mySerial.println("\t 'p' - Power mode (default drive function)");
+  mySerial.println("\t 'o' - Set Options");
+  mySerial.println("\t 'c' - Configuration");
+  mySerial.println("\t 'h' - This help screen");
+  mySerial.println("Power mode commands:");
+  mySerial.println("\t 'F | w' - Forward pulse");
+  mySerial.println("\t 'B | s' - Backward pulse");
+  mySerial.println("\t 'L | a' - Left pulse");
+  mySerial.println("\t 'R | d' - Right pulse");
+  // TODO: Support arrow keys
+}
+
+void optionSet(byte read) {
+	if(option == 'v') if (read > 0 && read < 255) velocity = read;
+	
+	mode = 'd'; // Go back to drive mode once the selected option is set
+}
+
+void config(byte read) {
+	option = read; // Keep track of what option is going to be set
+	mode = 'o'; // Put the program into option-set mode
+}
+
+void driveCommand(byte read) {
+	switch(read) {
+		case 'F':
+		case 'w': goFwd(); break;
+		case 'B':
+		case 's': goBack(); break;
+		case 'L':
+		case 'a': goLeft(); break;
+		case 'R':
+		case 'd': goRight(); break;
+	}
+}
+
+void goFwd() {
+Serial.println("Forward");
+analogWrite(leftFwd, velocity);
+analogWrite(rightFwd, velocity);
+delay(commandDelay);
+analogWrite(leftFwd, 0);
+analogWrite(rightFwd, 0);
+}
+
+void goBack() {
+Serial.println("Backward");
+analogWrite(leftRev, velocity);
+analogWrite(rightRev, velocity);
+delay(commandDelay);
+analogWrite(leftRev, 0);
+analogWrite(rightRev, 0);
+}
+
+void goLeft() {
+Serial.println("Left");
+analogWrite(leftRev, velocity);
+analogWrite(rightFwd, velocity);
+delay(commandDelay);
+analogWrite(leftRev, 0);
+analogWrite(rightFwd, 0);
+}
+
+void goRight() {
+Serial.println("Right");
+analogWrite(leftFwd, velocity);
+analogWrite(rightRev, velocity);
+delay(commandDelay);
+analogWrite(leftFwd, 0);
+analogWrite(rightRev, 0);
+}
